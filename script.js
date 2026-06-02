a47a613c4b63a395c287d31d734d6491ad29522e
fanesiodev-ui <fanesio.dev@gmail.com>
2026-06-02 09:29:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const menu = document.getElementById("menu");
+const menuToggle = document.getElementById("menuToggle");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+const links = document.querySelectorAll(".menu a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
+
